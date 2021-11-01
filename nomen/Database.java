881854bd0cc6e42980d037122498c0b602c9d190
ShadyKhaos <lpbalsamo@gmail.com>
2021-11-01 14:34:44 -0400

    created User class to save the access level
    Change-Id: I3243e1b6b797345c11522205ed9a10931bc24992

--- nomen/Database.java
+++ nomen/Database.java
@@ -5,7 +5,8 @@ import java.sql.*;
 public class Database {
 	
 	Connection con; //where to store results of quereies
-
+	User use = new User();
+	
 	public ResultSet employeeLookup(Connection db, String uName, String pass) {
 		String mainQuery = "SELECT E.Employee_Name, E.Employee_Password FROM Employees "
 				+ "E WHERE E.Employee_Name = " + "'" + uName + "'"
@@ -22,6 +23,37 @@ public class Database {
 		return queryOutput; 
 	}
 	
+	public boolean loginCheck(String uname, String pass) {
+		int uid = Integer.parseInt(uname);
+		String q = "SELECT Employee_ID, Employee_Password FROM Employees WHERE Employee_ID = "+uname+";";
+		PreparedStatement stat;
+		try {
+			stat = con.prepareStatement(q);
+			ResultSet set = stat.executeQuery();
+			set.next();
+			/*while(set.next()) {
+				System.out.print(set.getString(1)+" ");
+				System.out.print(set.getString(2));
+			}*/
+				if(set.getInt(1)==uid){
+					if(pass.equals(set.getString(2))) {
+						use.setaLv(uname);
+						return true;
+				}else {
+					return false;
+				}
+					
+				}else {
+					return false;
+				}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+		}
+	
+	
 	
 	public  void testq() {
 		PreparedStatement Q;
@@ -29,7 +61,14 @@ public class Database {
 			Q = con.prepareStatement("SELECT * FROM Employees;");
 			ResultSet res = Q.executeQuery();
 			while(res.next()) {
-				System.out.println(res);
+				System.out.print(res.getString(1)+" ");
+				System.out.print(res.getString(2)+" ");
+				System.out.print(res.getString(3)+" ");
+				System.out.print(res.getString(4)+" ");
+				System.out.print(res.getString(5)+" ");
+				System.out.print(res.getString(6)+" ");
+				System.out.print(res.getString(7)+" ");
+				System.out.println(res.getString(8));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -40,10 +79,41 @@ public class Database {
 	//Constructor that connects to database upon inilization 
 	public Database() {
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/nomenpayroll?allowPublicKeyRetrieval=true&useSSL=false", "root", "");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/nomenpayroll?allowPublicKeyRetrieval=true&useSSL=false", "emp", "pass");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	class User{
+		
+		int aLv;
+	
+		void tmp() {System.out.println("test");}
+		
+		void setaLv(String x) {
+			int uid = Integer.parseInt(x);
+			String q = "SELECT Employee_Position FROM Employees WHERE Employee_ID ="+uid+";";
+			PreparedStatement stat;
+			try {
+				stat = con.prepareStatement(q);
+				ResultSet set = stat.executeQuery();
+				set.next();
+				//System.out.println(set.getString(1)); //to show what querey gives
+				if(set.getString(1).equals("HR")) {
+					aLv = 1;
+				}else if(set.getString(1).equals("Manager")) {
+					aLv = 2;
+				}else if(set.getString(1).equals("Employee")) {
+					aLv = 3;
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
 }
--- nomen/Main.java
+++ nomen/Main.java
@@ -3,10 +3,12 @@ package nomen;
 public class Main extends UI{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//UI tmp = new UI();
-		//tmp.start();
-		Database db = new Database();
+		/*Database db = new Database(); //for testing db stuff
 		db.testq();
+		if(db.loginCheck("1","password")) {
+			System.out.print("it worked ");
+		}else
+			System.out.print("No work ");*/
 		launch(args);
 	}
 	
